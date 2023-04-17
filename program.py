e4d8831f85fb8de57d400bb15d5adab4eb0ce2b9
Niusha_ygn <niusha.yaghini@gmail.com>
2023-04-17 13:04:56 +0430

    back propagation

--- program.py
+++ program.py
@@ -54,4 +54,27 @@ class MLP(object):
         #return output layer activation
         return activations
         
+    
+    def back_propagate(self, loss, verbose=False):
+        
+        #dE/dw_i = (y - a_(i+1)) s'(h_(i+1)) a_i
+        #s'(h_(i+1)) = s(h_(i+1))(1 - s(h_(i+1)))
+        #s(h_(i+1)) = a_(i+1)
+        
+        #dE/dw_(i+1) = (y - a_(i+1)) s'(h_(i+1)) w_i s'(h_i) a_(i-1)
+        
+        for i in reversed(range(len(self.derivatives))):
+            activations = self.activations[i+1]
+            delta = loss * self._sigmoid_derivaties(activations) #NDarray((0.1, 0.2)) --> NDarray(((0.1, 0.2)))
+            delta_reshaped = delta.reshape(delta.shape[0], -1).T
+            current_activations = self.activations[i]  #NDarray((0.1, 0.2)) --> NDarray(((0.1), (0.2)))
+            current_activations_reshaped = current_activations.reshape(current_activations.shape[0], -1)
+            self.derivatives[i] = np.dot(current_activations_reshaped, delta_reshaped)
+            loss = np.dot(delta, self.weights[i].T)
+            
+            if verbose:
+                print("Derivatives for w{}: {}".format(i, self.derivatives[i]))
+                
+        return loss
+            
         

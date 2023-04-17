ad917fbe56a3cb2dcd93cea16a1f435d4080a031
Niusha_ygn <niusha.yaghini@gmail.com>
2023-04-17 12:48:50 +0430

    forward propagation

--- program.py
+++ program.py
@@ -1,8 +1,10 @@
 import numpy as np
 from random import random
 
-#a multilayer perception class
+
 class MLP(object):
+#a multilayer perception class
+
     def __init__(self, num_inputs=2, hidden_layers=[3, 3], num_outputs=1):
         self.num_inputs = num_inputs
         self.hidden_layers = hidden_layers
@@ -31,4 +33,25 @@ class MLP(object):
         self.derivatives = derivatives
         
         
-    
+    def forward_propagates(self, inputs):
+        #computes forward propagation of the network based on input
+        #Return: (NDarray) activations
+        
+        
+        #the input layer activation is just the input itself
+        activations = inputs
+        self.activations[0] = inputs
+        
+        #iterate through the network layers
+        for i, w in enumerate(self.weights):
+            #calculate matrix multiplication between previos activation and weight matrix
+            net_inputs = np.dot(activations, w)
+            
+            #apply sigmoid activation function
+            activations = self._sigmoid(net_inputs)
+            self.activations[i+1] = activations
+            
+        #return output layer activation
+        return activations
+        
+        
